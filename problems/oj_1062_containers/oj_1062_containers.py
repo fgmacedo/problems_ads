b3fa39a7d350744f5793e1960c8965663c35403b
Fernando Macedo <fgmacedo@gmail.com>
2021-04-03 17:42:28 -0300

    solution: accepted solution for oj_1062_containers.py in 0.010s

--- problems/oj_1062_containers/oj_1062_containers.py
+++ problems/oj_1062_containers/oj_1062_containers.py
@@ -8,3 +8,25 @@ import sys
 from collections import deque
 
 rl = sys.stdin.readline
+
+
+def handle(containers):
+    stacks = deque()
+    for ship in containers:
+        for stack in stacks:
+            if ship <= stack[-1]:
+                stack.append(ship)
+                break
+        else:  # for's else is only executed if the loop finishes without a break statement
+            stacks.append(deque([ship]))
+
+    return len(stacks)
+
+
+results = [
+    f"Case {idx}: {handle(case.rstrip())}"
+    for idx, case in enumerate(sys.stdin, 1)
+    if not case == "end\n"
+]
+
+print("\n".join(results))
