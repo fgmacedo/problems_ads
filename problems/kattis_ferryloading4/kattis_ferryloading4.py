a9ae2d065c9b254d5181198cfa00f8e038c1cbd4
Fernando Macedo <fgmacedo@gmail.com>
2021-04-04 18:58:36 -0300

    solution: kattis_ferryloading4

--- problems/kattis_ferryloading4/kattis_ferryloading4.py
+++ problems/kattis_ferryloading4/kattis_ferryloading4.py
@@ -8,3 +8,38 @@ import sys
 from collections import deque
 
 rl = sys.stdin.readline
+debug = "--debug" in sys.argv
+
+number_of_testcases = int(rl())
+
+
+for _ in range(number_of_testcases):
+    # l: the size in meters of the ferry (1 ≤ l ≤ 500)
+    # m: number of cars (1 ≤ m ≤ 10000)
+    l, m = map(int, rl().split())
+
+    number_of_cars = m
+    ferry_size = l * 100
+
+    banks = deque([deque(), deque()])
+    for _ in range(number_of_cars):
+        length, destiny = rl().split()
+        bank = banks[destiny != 'left']
+        bank.append(int(length))
+
+    iterations = 0
+    while any(banks):
+        occuped_space = 0
+        bank = banks[0]
+        while bank and occuped_space + bank[0] <= ferry_size:
+            occuped_space += bank.popleft()
+
+        # cycle banks
+        banks.append(banks.popleft())
+        iterations += 1
+
+    print(iterations)
+
+
+    if debug:
+        print(banks)
